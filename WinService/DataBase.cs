47167ec368c0ef10b17e8389d39ac311058ffbcf
Lokman Bozan <lbozan@outlook.com>
2018-07-20 15:10:31 +0300

    test project

---
+++ WinService/DataBase.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WinService
+{
+    public class DataBase
+    {
+        private static DataBase instans;
+        private List<TabloBir> _tablobir;
+        private List<TabloIki> _tabloiki;
+        private List<TabloUc> _tablouc;
+        private DataBase()
+        {
+            _tablobir = new List<TabloBir>();
+            _tabloiki = new List<TabloIki>();
+            _tablouc = new List<TabloUc>();
+        }
+        public void TabloFulle()
+        {
+            _tablobir.Add(new TabloBir() { Id = 1, X = "41.063244", Y = "21.992814" });
+            _tablobir.Add(new TabloBir() { Id = 2, X = "42.063244", Y = "22.992814" });
+            _tablobir.Add(new TabloBir() { Id = 3, X = "43.063244", Y = "23.992814" });
+            _tablobir.Add(new TabloBir() { Id = 4, X = "44.063244", Y = "24.992814" });
+            _tablobir.Add(new TabloBir() { Id = 5, X = "45.063244", Y = "25.992814" });
+            _tablobir.Add(new TabloBir() { Id = 6, X = "46.063244", Y = "26.992814" });
+            _tablobir.Add(new TabloBir() { Id = 7, X = "47.063244", Y = "27.992814" });
+            _tablobir.Add(new TabloBir() { Id = 8, X = "48.063244", Y = "28.992814" });
+            _tablobir.Add(new TabloBir() { Id = 9, X = "49.063244", Y = "29.992814" });
+
+            _tabloiki.Add(new TabloIki() { Id = 1, X = "51.063244", Y = "21.992814" });
+            _tabloiki.Add(new TabloIki() { Id = 2, X = "52.063244", Y = "22.992814" });
+            _tabloiki.Add(new TabloIki() { Id = 3, X = "53.063244", Y = "23.992814" });
+            _tabloiki.Add(new TabloIki() { Id = 4, X = "54.063244", Y = "24.992814" });
+            _tabloiki.Add(new TabloIki() { Id = 5, X = "55.063244", Y = "25.992814" });
+            _tabloiki.Add(new TabloIki() { Id = 6, X = "56.063244", Y = "26.992814" });
+            _tabloiki.Add(new TabloIki() { Id = 7, X = "57.063244", Y = "27.992814" });
+            _tabloiki.Add(new TabloIki() { Id = 8, X = "58.063244", Y = "28.992814" });
+            _tabloiki.Add(new TabloIki() { Id = 9, X = "59.063244", Y = "29.992814" });
+        }
+        public List<TabloBir> TabloBirList()
+        {
+            return _tablobir.ToList();
+        }
+        public List<TabloIki> TabloIkiList()
+        {
+            return _tabloiki.ToList();
+        }
+        public List<TabloUc> Liste
+        {
+            get
+            {
+                return _tablouc.ToList();
+            }
+        }
+        public void TabloUcEkle(TabloUc data)
+        {
+            _tablouc.Add(data);
+        }
+        public static DataBase Instans
+        {
+            get
+            {
+                if (instans == null)
+                {
+                    instans = new DataBase();
+                }
+                return instans;
+            }
+        }
+    }
+
+    public class TabloBir
+    {
+        public int Id { get; set; }
+        public string X { get; set; }
+        public string Y { get; set; }
+    }
+    public class TabloIki
+    {
+        public int Id { get; set; }
+        public string X { get; set; }
+        public string Y { get; set; }
+    }
+    public class TabloUc
+    {
+        public int Id { get; set; }
+        public string X { get; set; }
+        public string Y { get; set; }
+        public string Adres { get; set; }
+    }
+}
---
+++ WinService/Service1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.ServiceProcess;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace WinService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+        private static DataBase _data;
+        private static string path;
+        protected override void OnStart(string[] args)
+        {
+            path = $"{AppDomain.CurrentDomain.BaseDirectory}GeoFile.txt";
+            _data = DataBase.Instans;
+            _data.TabloFulle();
+            Thread thx = new Thread(new ThreadStart(Bir));
+            Thread thv = new Thread(new ThreadStart(Iki));
+            thx.Start();
+            thv.Start();
+        }
+
+        private static string GeoData(string x, string y)
+        {
+            var request = (HttpWebRequest)WebRequest.Create($"http://maps.google.com/maps/api/geocode/xml?latlng={x},{y}&sensor=false");
+            var response = (HttpWebResponse)request.GetResponse();
+            Stream resultStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(resultStream);
+            string finish = reader.ReadToEnd();
+            string pattern = @"<formatted_address>(.*)<\/formatted_address>";
+            Regex rgx = new Regex(pattern, RegexOptions.IgnoreCase);
+            Match m = rgx.Match(finish);
+            string son = m.Groups[0].ToString().Replace("<formatted_address>", "").Replace("</formatted_address>", "");
+            return son;
+        }
+
+        private static void Bir()
+        {
+            _data.TabloBirList().ForEach(v =>
+            {
+                string adres = GeoData(v.X, v.Y);
+                TabloUc newData = new TabloUc()
+                {
+                    X = v.X,
+                    Y = v.Y,
+                    Adres = adres
+                };
+                _data.TabloUcEkle(newData);
+                Console.WriteLine($"X : {newData.X}\t Y : {newData.Y}\t Adres : {newData.Adres}");
+                Thread.Sleep(4000);
+            });
+        }
+        private static void Iki()
+        {
+            _data.TabloIkiList().ForEach(v => {
+                string adres = GeoData(v.X, v.Y);
+                TabloUc newData = new TabloUc()
+                {
+                    X = v.X,
+                    Y = v.Y,
+                    Adres = adres
+                };
+                _data.TabloUcEkle(newData);
+                Console.WriteLine($"X : {newData.X}\t Y : {newData.Y}\t Adres : {newData.Adres}");
+                Thread.Sleep(6000);
+            });
+            var asd = _data.Liste;
+
+
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    _data.Liste.ForEach(v =>
+                    {
+                        sw.WriteLine($"X : {v.X}\t Y : {v.Y}\t Adres : {v.Adres}");
+                    });
+                }
+            }
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                _data.Liste.ForEach(v =>
+                {
+                    sw.WriteLine($"X : {v.X}\t Y : {v.Y}\t Adres : {v.Adres}");
+                });
+            }
+        }
+        
+        protected override void OnStop()
+        {
+            Console.WriteLine("On Stop");
+        }
+    }
+}
